a0d0867410b67ed1134bf3bffa9ecf0937d2d37a
Mitzi Estrada <m_estrada17@live.com>
2020-03-04 17:37:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :name
+  
+  def initialize(word)
+    @name = word
+  end
+  
+  def match(arr)
+    arr.select {|x| x.split("").sort == @name.split("").sort}
+  end
+  
+  
+end
